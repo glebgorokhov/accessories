0f2cf85afc19de3916631114a753e19507c2a916
Gleb Gorokhov <gg@verstaburo.com>
2019-10-03 07:06:18 +0300

    Страница товара (full)

--- app/blocks/slider/slider.js
+++ app/blocks/slider/slider.js
@@ -7,33 +7,41 @@ const $ = window.$;
 export function slider() {
   $('.js-slider').each(function () {
     const
-      block = $(this);
+      block = $(this),
+      top = block.find('.js-slider-top'),
+      bottom = block.find('.js-slider-bottom');
 
-    const mySlider = new Swiper(block, {
-      loop: true,
+    const bottomSlider = new Swiper(bottom, {
+      loop: false,
       speed: 700,
-      autoplay: {
-        delay: 2000,
-      },
+      autoplay: false,
+      slidesPerView: 5,
+      centeredSlides: false,
+      roundLengths: true,
+      freeMode: false,
+      watchSlidesVisibility: true,
+      watchSlidesProgress: true,
+      spaceBetween: -1,
+      on: {
+        init: function (info) {
+          if (this.slides.length < 5) {
+            bottom.find('.swiper-wrapper').addClass('swiper-wrapper_center');
+          }
+        }
+      }
+    });
+
+    const topSlider = new Swiper(top, {
+      loop: false,
+      speed: 700,
+      autoplay: false,
       slidesPerView: 1,
       spaceBetween: 0,
       centeredSlides: false,
       roundLengths: true,
       freeMode: false,
-      navigation: {
-        nextEl: block.find('.slider__button_next'),
-        prevEl: block.find('.slider__button_prev'),
-      },
-      pagination: {
-        el: block.find('.slider__dots'),
-        clickable: true,
-        bulletClass: 'slider__dot',
-        bulletActiveClass: 'is-active',
-      },
-      breakpoints: {
-        768: {
-          slidesPerView: 2,
-        },
+      thumbs: {
+        swiper: bottomSlider,
       },
     });
   });
